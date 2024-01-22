a535226eb2185e88d3ca4bb173e9c39bc6d2b4ad
Thiago Gasparini <thiago_engprod@outlook.com>
2024-01-22 20:15:24 -0300

    importando dados dinamicamente

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
+  name: string = "Thiago";
+  age: number = 30;
+  car = {
+    name: "Gol G4",
+    year: 2013,
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
 
 }
