29709c68486a739a1b0da752e68e61a9194e70f9
Thiago Gasparini <thiago_engprod@outlook.com>
2024-01-22 21:30:15 -0300

    compartilhamento de dados com @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  userName = 'Gasparini';
+
+  userData = {
+    email: 'thiago_engprod@outlook.com',
+    role: 'admin',
+  };
+
   title = 'curso-angular';
 }
