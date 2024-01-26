af2ff027b8340d19311555263926d5358607db87
Thiago Gasparini <thiago_engprod@outlook.com>
2024-01-26 16:32:53 -0300

    two-way-binding forms

---
+++ src/app/components/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-two-way-binding',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './two-way-binding.component.html',
+  styleUrl: './two-way-binding.component.css'
+})
+export class TwoWayBindingComponent {
+  name: string = '';
+
+  clearInput(): void {
+    alert('Input Limpo')
+    this.name = '';
+  }
+
+}
