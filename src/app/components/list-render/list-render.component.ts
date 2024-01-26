95d3a8251a1b39a604f23260b6a77a3d1ae8ed93
Thiago Gasparini <thiago_engprod@outlook.com>
2024-01-26 15:12:10 -0300

    interfaces no angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from '../../Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -8,10 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './list-render.component.css',
 })
 export class ListRenderComponent {
-  animals = [
-    { id: 1, name: 'Turca', type: 'Dog' },
-    { id: 2, name: 'Tom', type: 'Cat' },
-    { id: 3, name: 'Frida', type: 'Dog' },
-    { id: 4, name: 'Bob', type: 'Horse' },
+  animals: Animal[] = [
+    { id: 1, name: 'Turca', type: 'Dog', age: 5 },
+    { id: 2, name: 'Tom', type: 'Cat', age: 3 },
+    { id: 3, name: 'Frida', type: 'Dog', age: 1 },
+    { id: 4, name: 'Bob', type: 'Horse', age: 8 },
   ];
+
+  animal: Animal = {
+    id: 1,
+    name: 'Lupi',
+    type: 'Dog',
+    age: 15,
+  };
+
+  animalAge: string = '';
+
+  showAge(animal: Animal): void {
+    this.animalAge = `O pet ${animal.name} tem ${animal.age}`
+  }
+
+  clearInfo(): void {
+    this.animalAge = '';
+  }
 }
